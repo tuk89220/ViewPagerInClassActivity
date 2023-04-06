edefe0dd50e719819c6ac3a802eebd6f536ca421
Ben-Foran <tuk89220@temple.edu>
2023-04-06 15:20:30 -0400

    Added page creation on button click and jump to latest added page

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -3,32 +3,46 @@ package edu.temple.viewpagerinclasssctivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
-
+    private val addButton: Button by lazy{
+        findViewById(R.id.addButton)
+    }
     private val viewPager: ViewPager2 by lazy{
         findViewById(R.id.viewPager)
     }
+    private var numberOfPages = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager.adapter = object: FragmentStateAdapter(this){
-            override fun getItemCount() = 10
-
-            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
-
-            //personal notes for assignment
-        // 8: do not have webView be destroyed in certain circumstances on rotation. Stop activity from restarting
-            //in manifest in <activity - android.configChanges = "orientation|keyboardHidden|screenSize">
-
-
+        addButton.setOnClickListener() {
+            numberOfPages++
+            viewPager.adapter?.notifyItemInserted(numberOfPages - 1)
+            viewPager.setCurrentItem(numberOfPages,true)
+            //personal notes for assignment 8:
+            //used to contact a specific fragment - supportFragmentManager.findFragmentByTag("f" + position)
         }
 
-    }
+            viewPager.adapter = object : FragmentStateAdapter(this) {
+                override fun getItemCount() = numberOfPages
+
+                override fun createFragment(position: Int) =
+                    TextFragment.newInstance((position + 1).toString())
+
+                //personal notes for assignment 8:
+                //do not have webView be destroyed in certain circumstances on rotation. Stop activity from restarting
+                //in manifest in <activity - android.configChanges = "orientation|keyboardHidden|screenSize">
+                //with previous adapters, we always used collections, with fragment state adapter there is no defined collection
+                //fragment manager hold the collections,
+
+            }
+
+        }
 
 }
